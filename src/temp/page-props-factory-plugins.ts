34c33222a0ba18cf58ae2c218ec0c5dcf8d02799
Ramiro Batallas <rbatallas@oshyn.com>
2024-11-23 19:00:53 -0500

    add all files

---
+++ src/temp/page-props-factory-plugins.ts
@@ -0,0 +1,5 @@
+export { componentPropsPlugin } from 'src/lib/page-props-factory/plugins/component-props';
+export { contentStylesPlugin } from 'src/lib/page-props-factory/plugins/content-styles';
+export { normalModePlugin } from 'src/lib/page-props-factory/plugins/normal-mode';
+export { previewModePlugin } from 'src/lib/page-props-factory/plugins/preview-mode';
+export { sitePlugin } from 'src/lib/page-props-factory/plugins/site';
