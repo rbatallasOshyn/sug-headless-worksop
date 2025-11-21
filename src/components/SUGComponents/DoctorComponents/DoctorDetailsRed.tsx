ceb4aab3b1063dc30b53908d6f83d9e281097788
Gabo <gabrielbaldeon@gmail.com>
2025-11-20 20:17:39 -0500

    New component

---
+++ src/components/SUGComponents/DoctorComponents/DoctorDetailsRed.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Text,
+  RichText,
+  NextImage,
+  TextField,
+  RichTextField,
+  ImageField,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+import styles from './DoctorDetailsRed.module.css';
+
+type DoctorDetailsRedProps = {
+  fields: {
+    Name: TextField;
+    Content: RichTextField;
+    Photo: ImageField;
+  };
+};
+
+const DoctorDetailsRed = (props: DoctorDetailsRedProps): JSX.Element => {
+  const { fields } = props;
+
+  if (!fields) return <></>;
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.plasmaCard}>
+        <div className={styles.left}>
+          <h2 className={styles.title}>
+            <Text field={fields.Name} />
+          </h2>
+          <div className={styles.contentText}>
+            <RichText field={fields.Content} />
+          </div>
+        </div>
+
+        {fields.Photo && (
+          <div className={styles.right}>
+            <NextImage
+              field={fields.Photo}
+              width={600}
+              height={400}
+              style={{ width: '100%', height: 'auto', borderRadius: '12px' }}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DoctorDetailsRed;
